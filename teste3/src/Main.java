bca517d7ce7de866f1996baaca8a0d7c9269d6d4
Gabriel Conceicao | BRASFORT <gabriel.conceicao@brasfort.com.br>
2026-04-23 16:51:29 -0300

    Arquivos do projeto

---
+++ teste3/src/Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] mat = new int[m][n];
+
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[i].length; j++){
+                System.out.println();
+                System.out.println("Posição: " + i + "," + j + ":");
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println();
+
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[i].length; j++){
+                System.out.println(mat[i][j]);
+            }
+        }
+
+        System.out.println();
+        System.out.println("Escolha um número da Matriz para saber os valores em volta: ");
+        int x = sc.nextInt();
+
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[i].length; j++){
+                if(mat[i][j] == x){
+                    System.out.println();
+                    System.out.println("Posição " + i + "," + j + ":" );
+                    if(j > 0){
+                        System.out.println("Esquerda: " + mat[i][j - 1]);
+                    }
+                    if(j < mat[i].length - 1){
+                        System.out.println("Direita: " + mat[i][j + 1]);
+                    }
+                    if(i < mat.length - 1){
+                        System.out.println("Baixo: " + mat[i + 1][j]);
+                    }
+                    if(i > 0){
+                        System.out.println("Cima: " + mat[i - 1][j]);
+                    }
+                }
+            }
+        };
+
+        sc.close();
+    }
+}
